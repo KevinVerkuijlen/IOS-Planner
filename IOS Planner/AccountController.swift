05afe62e2de136cc2ca7941a2e484cd6d137518e
Kevin Verkuijlen <Kevin@aegis-2.local>
2016-03-09 09:57:13 +0100

    CodeBehind added to First Screens

---
+++ IOS Planner/AccountController.swift
@@ -0,0 +1,26 @@
+//
+//  AccountController.swift
+//  IOS Planner
+//
+//  Created by Kevin Verkuijlen on 09-03-16.
+//  Copyright © 2016 Kevin Verkuijlen. All rights reserved.
+//
+
+import UIKit
+
+class AccountController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+        //
+    }
+    
+    
+}
---
+++ IOS Planner/AgendaController.swift
@@ -0,0 +1,26 @@
+//
+//  AgendaController.swift
+//  IOS Planner
+//
+//  Created by Kevin Verkuijlen on 09-03-16.
+//  Copyright © 2016 Kevin Verkuijlen. All rights reserved.
+//
+
+import UIKit
+
+class AgendaController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+        //
+    }
+    
+    
+}
